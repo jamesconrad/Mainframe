95c2d9e30f7d531e64ff63419a374e84e0c1fffc
jamesconrad <james.conrad@uoit.net>
2014-09-21 19:25:13 -0400

    Added chat box to border

--- DevTitle/conbufferclass.cpp
+++ DevTitle/conbufferclass.cpp
@@ -90,22 +90,72 @@ int ConBufferClass::InitiazlizeBorder()
 	for (int index = 0; index <= SCREEN_HEIGHT*SCREEN_WIDTH; index++)
 	{//9619 for testing
 		if (index == 0) ConBufferClass::border[index].Char.UnicodeChar = 9556;
-		else if (index == SCREEN_WIDTH - 1) ConBufferClass::border[index].Char.UnicodeChar = 9559;
-		else if (index == SCREEN_HEIGHT*SCREEN_WIDTH - SCREEN_WIDTH) ConBufferClass::border[index].Char.UnicodeChar = 9562;
-		else if (index == SCREEN_HEIGHT*SCREEN_WIDTH - 1) ConBufferClass::border[index].Char.UnicodeChar = 9565;
-		else if (index == 49) ConBufferClass::border[index].Char.UnicodeChar = 9574;
-		else if (index == SCREEN_WIDTH * 33) ConBufferClass::border[index].Char.UnicodeChar = 9568;
-		else if (index == SCREEN_WIDTH * 34 - 1) ConBufferClass::border[index].Char.UnicodeChar = 9571;
-		else if (index == SCREEN_WIDTH * 33 + 49) ConBufferClass::border[index].Char.UnicodeChar = 9577;
+		else if (index == SCREEN_WIDTH - 1) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9559;
+		else if (index == SCREEN_HEIGHT*SCREEN_WIDTH - SCREEN_WIDTH) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9562;
+		else if (index == SCREEN_HEIGHT*SCREEN_WIDTH - 1) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9565;
+		else if (index == 49) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9574;
+		else if (index == SCREEN_WIDTH * 33) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9568;
+		else if (index == SCREEN_WIDTH * 34 - 1) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9571;
+		else if (index == SCREEN_WIDTH * 33 + 49) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9577;
+		//Hardcode of chatbox
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 4) - 1) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9571;
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 3)) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9568;
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 3) + 6) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9574;
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 1) + 6) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9577;
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 2) + 6) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9553;
+		else if (index >= SCREEN_WIDTH * 48 + 1 && index <= SCREEN_WIDTH * 49 - 2) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9552;
+		//Chat:
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 2) + 1)
+		{
+			ConBufferClass::border[index].Char.UnicodeChar = 67;
+			ConBufferClass::border[index].Attributes = 0x0007;
+		}
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 2) + 2)
+		{
+			ConBufferClass::border[index].Char.UnicodeChar = 104;
+			ConBufferClass::border[index].Attributes = 0x0007;
+		}
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 2) + 3)
+		{
+			ConBufferClass::border[index].Char.UnicodeChar = 97;
+			ConBufferClass::border[index].Attributes = 0x0007;
+		}
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 2) + 4)
+		{
+			ConBufferClass::border[index].Char.UnicodeChar = 116;
+			ConBufferClass::border[index].Attributes = 0x0007;
+		}
+		else if (index == SCREEN_WIDTH * (SCREEN_HEIGHT - 2) + 5)
+		{
+			ConBufferClass::border[index].Char.UnicodeChar = 58;
+			ConBufferClass::border[index].Attributes = 0x0007;
+		}
 		//is it in row 1 or 33 or SCREEN_HEIGHT?
-		else if (index < SCREEN_WIDTH || (index > SCREEN_WIDTH * 33 && index < SCREEN_WIDTH * 34 - 1) || ((index > (SCREEN_HEIGHT - 1) * SCREEN_WIDTH && index < SCREEN_HEIGHT * SCREEN_WIDTH - 1))) ConBufferClass::border[index].Char.UnicodeChar = 9552;
+		else if (index < SCREEN_WIDTH || (index > SCREEN_WIDTH * 33 && index < SCREEN_WIDTH * 34 - 1) || (index > (SCREEN_HEIGHT - 1) * SCREEN_WIDTH && index < SCREEN_HEIGHT * SCREEN_WIDTH - 1)) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9552;
 		//is it in col 49 and above the middle bar?
-		else if (index >= SCREEN_WIDTH && index % SCREEN_WIDTH == 49 && index < SCREEN_WIDTH * 34) ConBufferClass::border[index].Char.UnicodeChar = 9553;
+		else if (index >= SCREEN_WIDTH && index % SCREEN_WIDTH == 49 && index < SCREEN_WIDTH * 34) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9553;
 		//is it in col 1 or SCREEN_WIDTH
-		else if (index >= SCREEN_WIDTH && index <= SCREEN_WIDTH*SCREEN_HEIGHT && (index % SCREEN_WIDTH == 0 || index % SCREEN_WIDTH == SCREEN_WIDTH - 1)) ConBufferClass::border[index].Char.UnicodeChar = 9553;
-		else ConBufferClass::border[index].Char.UnicodeChar = 32;
-
-		ConBufferClass::border[index].Attributes = 0x0004 | 0x0008;
+		else if (index >= SCREEN_WIDTH && index <= SCREEN_WIDTH*SCREEN_HEIGHT && (index % SCREEN_WIDTH == 0 || index % SCREEN_WIDTH == SCREEN_WIDTH - 1)) 
+			ConBufferClass::border[index].Char.UnicodeChar = 9553;
+		else 
+			ConBufferClass::border[index].Char.UnicodeChar = 32;
+		if (ConBufferClass::border[index].Attributes != 0x0007)
+			ConBufferClass::border[index].Attributes = 0x0004 | 0x0008;
 	}
 	return 1;
 }
