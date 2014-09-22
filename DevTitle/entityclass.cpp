c9bba19e111f6f634517bbcf3ed233e36c7ee496
jamesconrad <james.conrad@uoit.net>
2014-09-22 14:39:52 -0400

    Addition of entityclass, as it had not been commited previously

---
+++ DevTitle/entityclass.cpp
@@ -0,0 +1,48 @@
+#include <Windows.h>
+#include "entityclass.h"
+
+EntityClass::EntityClass()
+{
+}
+
+EntityClass::EntityClass(const EntityClass&)
+{
+}
+
+EntityClass::~EntityClass()
+{
+}
+
+int EntityClass::SetUnitData(UnitData unitData)
+{
+	EntityClass::unitData = unitData;
+	return 1;
+}
+
+
+int EntityClass::Initialize(UnitData unitData)
+{
+	EntityClass::unitData = unitData;
+	return 1;
+}
+
+int MoveUnit(char direction)
+{
+	//check to see if space is occupied
+
+	return 1;
+}
+
+int AttackUnit(UnitData srcUnit, char direction)
+{
+	if (srcUnit.actions = 0)
+	{
+		return 0;
+	}
+	for (int i = 0; i < srcUnit.range; i++)
+	{
+
+	}
+	srcUnit.actions = 0;
+	return 1;
+}
---
+++ DevTitle/entityclass.h
@@ -0,0 +1,22 @@
+#ifndef ENTITYCLASS_H
+#define ENTITYCLASS_H
+
+#include <Windows.h>
+#include "unitinfoclass.h"
+
+class EntityClass
+{
+public:
+	UnitData unitData;
+	EntityClass();
+	EntityClass(const EntityClass&);
+	~EntityClass();
+
+	int Initialize(UnitData unitData);
+	int MoveUnit(char direction);
+	int AttackUnit(UnitData src, char direction);
+	int SetUnitData(UnitData unitData);
+
+};
+
+#endif
